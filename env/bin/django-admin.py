13858306e6089a0346fe4ab6622b1c7ef2ebc17d
GuillermoECS <guillermoestebancs@gmail.com>
2018-08-31 21:57:37 -0600

    Python virtualenv

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/esteban/Desktop/SOLVO/Proyecto/Herramientas/Django/Django-Tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
