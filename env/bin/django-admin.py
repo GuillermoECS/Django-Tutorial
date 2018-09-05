40c1e676405ca536f3a6eb5dbec0c419737f3e0e
GuillermoECS <guillermoestebancs@gmail.com>
2018-09-05 11:22:58 -0600

    remove env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/esteban/Desktop/SOLVO/Proyecto/Herramientas/Django/Django-Tutorial/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
