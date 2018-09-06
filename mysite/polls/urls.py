70aa2c32d7b752aec1a75f18c9ac63ae47f7c203
GuillermoECS <guillermoestebancs@gmail.com>
2018-09-05 21:05:46 -0600

    Writing your first Django app, part 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+
+# Views "The Hard Way"
+'''
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
@@ -14,4 +17,15 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+'''
+
+# Generic Views
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(),
+         name='detail'),  # Question id to pk
+    path('<int:pk>/results/', views.ResultsView.as_view(),
+         name='results'),  # Question id to pk
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # No generic
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,62 +6,111 @@ from .models import Question  # Question
 from django.template import loader  # Loader of template
 from django.shortcuts import render  # Function render
 from django.shortcuts import get_object_or_404  # get_object_or_404()
-
-# Pagina de inicio
-
-
-def index(request):
-    # Without template: Carga las preguntas, separadas por coma
-    """
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-    """
-    # With template: Carga las preguntas, haciendo uso de plantillas
-    '''
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-    '''
-    # With template, using function render
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-# Pagina de detalle de la pregunta, requiere parametros
+from django.urls import reverse  # Form
+from .models import Choice  # Form
+from django.http import HttpResponseRedirect  # Form
+from django.views import generic  # Generic Views
 
 
-def detail(request, question_id):
-    # Without template: Carga el id de la pregunta seleccionada
-    '''
-    return HttpResponse("You're looking at question %s." % question_id)
-    '''
-    # With template: Carga la pregunta y validacion si no se encuentra
-    '''
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-    '''
-    # With template: Carga la pregunta y validacion por si no se encuentra
-    # Por medio de una funcion
-    choice = {}
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question, 'choice': choice})
+# View "The Hard Way"
+###############################################################################
+# def index(request):  # Pagina de inicio
+#    # Without template: Carga las preguntas, separadas por coma
+#    """
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    output = ', '.join([q.question_text for q in latest_question_list])
+#    return HttpResponse(output)
+#    """
+#    # With template: Carga las preguntas, haciendo uso de plantillas
+#    '''
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    return HttpResponse(template.render(context, request))
+#    '''
+#    # With template, using function render
+#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
 
-# Pagina de resultados de la pregunta, requiere parametros
+
+# View "The Hard Way"
+# def detail(request, question_id):  # Pagina de detalle de la pregunta, requiere parametros
+#    # Without template: Carga el id de la pregunta seleccionada
+#    '''
+#    return HttpResponse("You're looking at question %s." % question_id)
+#    '''
+#    # With template: Carga la pregunta y validacion si no se encuentra
+#    '''
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'polls/detail.html', {'question': question})
+#    '''
+#    # With template: Carga la pregunta y validacion por si no se encuentra
+#    # Por medio de una funcion
+#    choice = {}
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question, 'choice': choice})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# View "The Hard Way"
+# def results(request, question_id):  # Pagina de resultados de la pregunta, requiere parametros
+#    # Dummy implementation
+#    '''
+#    response = "You're looking at the results of question %s."
+#    return HttpResponse(response % question_id)
+#    '''
+#    # Real implementation
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
+
+# View "The Hard Way"
 # Votos para la pregunta, requiere parametros
-
-
-def vote(request, question_id):
+'''
+ def vote(request, question_id):  # Dummy implementation of vote()
     return HttpResponse("You're voting on question %s." % question_id)
+'''
+
+
+def vote(request, question_id):  # Real implementation of vote()
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+# Generic Views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
